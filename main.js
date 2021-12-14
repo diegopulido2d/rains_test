0c0768bf9048197a412863b1ec04759b0201a7fd
Diego Pulido <diego.pulido@ariadnacg.com>
2021-12-14 02:12:20 -0500

    12/14

--- main.js
+++ main.js
@@ -39,6 +39,10 @@ window.addEventListener('load', function(e){
         title.innerHTML = data[i]['title'];
         let info = document.createElement('div');
         info.classList.add('info');
+        let curr = document.createElement('span');
+        let pt = document.createElement('div');
+        curr.classList.add('curr');
+        curr.innerHTML = '$'
         let price = document.createElement('span');
         price.classList.add('price');
         price.innerHTML = data[i]['price'];
@@ -53,7 +57,10 @@ window.addEventListener('load', function(e){
 
         card.appendChild(imgCont);
         card.appendChild(title);
-        info.appendChild(price);
+        info.appendChild(curr);
+        pt.appendChild(curr);
+        pt.appendChild(price);
+        info.appendChild(pt);
         info.appendChild(rating);
         rating.appendChild(rate);
         rating.appendChild(ct);
@@ -86,6 +93,10 @@ window.addEventListener('load', function(e){
           title.innerHTML = data[i]['title'];
           let info = document.createElement('div');
           info.classList.add('info');
+          let curr = document.createElement('span');
+          let pt = document.createElement('div');
+          curr.classList.add('curr');
+          curr.innerHTML = '$'
           let price = document.createElement('span');
           price.classList.add('price');
           price.innerHTML = data[i]['price'];
@@ -100,7 +111,10 @@ window.addEventListener('load', function(e){
   
           card.appendChild(imgCont);
           card.appendChild(title);
-          info.appendChild(price);
+          info.appendChild(curr);
+          pt.appendChild(curr);
+          pt.appendChild(price);
+          info.appendChild(pt);
           info.appendChild(rating);
           rating.appendChild(rate);
           rating.appendChild(ct);
@@ -119,10 +133,6 @@ window.addEventListener('load', function(e){
 
 
 
-
-
-
-
 function dropMenu(){
     let menuMobile = document.getElementsByClassName('mobile')[0];
 
@@ -135,4 +145,60 @@ function dropMenu(){
     }
 
     
+}
+
+
+
+
+function euro(){
+
+    ajaxGet("https://openexchangerates.org/api/latest.json?app_id=c6981396ff044244bcf3fb8542942fec", function (response) {
+
+        let data = JSON.parse(response);
+        let conversion = data['rates']['EUR'];
+
+        let prices = document.getElementsByClassName('price');
+        let currs = document.getElementsByClassName('curr');
+        let priceCount = Object.keys(prices).length;
+
+        for (let i = 0; i < priceCount; i++){
+            let dollarPrice = prices[i].innerHTML;
+            let euroPrice = dollarPrice*conversion;
+
+            currs[i].innerHTML = '€';
+            prices[i].innerHTML = euroPrice.toFixed(2);
+        }
+
+        document.getElementsByClassName('euro')[0].style.display = 'none';
+        document.getElementsByClassName('dollar')[0].style.display = 'block';
+
+    });
+
+}
+
+
+function dollar(){
+
+    ajaxGet("https://openexchangerates.org/api/latest.json?app_id=c6981396ff044244bcf3fb8542942fec", function (response) {
+
+        let data = JSON.parse(response);
+        let conversion = data['rates']['EUR'];
+
+        let prices = document.getElementsByClassName('price');
+        let currs = document.getElementsByClassName('curr');
+        let priceCount = Object.keys(prices).length;
+
+        for (let i = 0; i < priceCount; i++){
+            let euroPrice = prices[i].innerHTML;
+            let dollarPrice = euroPrice/conversion;
+
+            currs[i].innerHTML = '$';
+            prices[i].innerHTML = dollarPrice.toFixed(2);
+        }
+
+        document.getElementsByClassName('euro')[0].style.display = 'block';
+        document.getElementsByClassName('dollar')[0].style.display = 'none';
+
+    });
+
 }
